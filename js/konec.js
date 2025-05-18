036ba3720ea904c2a7ee52bb0c17f77a0d4d48cc
037oo <leo123.svagelj@gmail.com>
2025-05-18 16:24:09 +0200

    Add files via upload

---
+++ js/konec.js
@@ -0,0 +1,16 @@
+var secondsLeft = 10;
+function redirTimer() {
+    setInterval(function () {
+        if (secondsLeft == 0) {
+            location.href = "index.php";
+        }
+        else {
+            document.getElementById("time").innerHTML = --secondsLeft;
+        }
+    }, 1000);
+
+
+}
+
+
+
